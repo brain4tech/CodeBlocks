b5ba789327c37e04f685fd5b6c0ebca608db7ac5
Agnes Widera <agnes.widera@studmail.w-hs.de>
2022-06-26 19:05:33 +0200

    Working code formatting ONLY ON MY COMPUTER

--- Source/TuringSandbox/CodeFormatting.cpp
+++ Source/TuringSandbox/CodeFormatting.cpp
@@ -7,26 +7,41 @@
 FText UCodeFormatting::FormatText(FString InputPath) {
 	UE_LOG(LogTemp, Warning, TEXT("Code Formatting: About to format from: \"%s\""), *InputPath);
 
-	// build command
-	// std::string Command = "bin\\clang-format-12.0.1_windows-amd64.exe --style=llvm input.txt";
-	std::string Command = "bin\\clang-format-12.0.1_windows-amd64.exe --help";
-	// Command += std::string(TCHAR_TO_UTF8(*InputPath));
+	// prepare input-directory
+	std::string InputDir = std::string(TCHAR_TO_UTF8(*InputPath));
 	
+	size_t pos;
+	while ((pos = InputDir.find("/")) != std::string::npos) {
+		InputDir.replace(pos, 1, "\\");
+	}
+
+	// prepare execution directory
+	FString TempExecDir = FPaths::ProjectDir();
+	TempExecDir.Append("Source/bin/clang-format-12.0.1_windows-amd64.exe");
+	std::string ExecDir = std::string(TCHAR_TO_UTF8(*TempExecDir));
+
+	while ((pos = ExecDir.find("/")) != std::string::npos) {
+		ExecDir.replace(pos, 1, "\\");
+	}
+ 
+
+	// build command
+	// std::string Command = "\"" + ExecDir + "\" --style=llvm -i \"" + InputDir + "\"";
+	std::string Command = "..\\..\\..\\..\\..\\..\\Users\\mariu\\Documents\\\"Unreal Projects\"\\\"TuringSandbox 5.0\"\\Source\\TuringSandbox\\bin\\clang-format-12.0.1_windows-amd64.exe --style=llvm -i \"" + InputDir + "\"";
+
+	// log command
 	FString CommandString(Command.c_str());
 	UE_LOG(LogTemp, Warning, TEXT("Code Formatting: Running Command: \"%s\""), *CommandString);
 
 	// run command
-	std::stringstream Output;
-	Output << system(Command.c_str()) << std::endl;
+	system(Command.c_str());
 
-	std::string OutputString = Output.str();
-	FString OutputFString(OutputString.c_str());
-	
-	UE_LOG(LogTemp, Warning, TEXT("Code Formatting: Received output: \"%s\""), *OutputFString);
+	// fetch the result
+	FString FileContent;
+	FFileHelper::LoadFileToString(FileContent, *InputPath,FFileHelper::EHashOptions::None);
 
 	// return output as FText
-	const FString OutString(Output.str().c_str());
-	return FText::FromString(OutString);
+	return FText::FromString(FileContent);
 }
 
 FString UCodeFormatting::SaveTextToFile(FString Content) {
